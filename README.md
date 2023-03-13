9659f910f174a750dfa94ae30013cbf66711b0c7
juanantoniosilva90 <j.a.silva.f90@gmail.com>
2023-03-13 12:01:10 -0600

    Primera subida

---
+++ README.md
@@ -0,0 +1,1 @@
+# Este es un módulo sencillo hecho con Thymeleaf y Java 8
---
+++ src/main/java/com/persona/SpringModuloThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.persona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModuloThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModuloThymeleafApplication.class, args);
+	}
+
+}
